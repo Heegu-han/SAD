553b7de67c6e1fecd728b5cf173d99fb97ecf69e
Heegu-han <80305565+Heegu-han@users.noreply.github.com>
2021-06-01 14:47:35 +0900

    generate python code

---
+++ python_code/car/Sonata.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Sonata:
+    def __init__(self):
+        self.speed = None
+
+    def start(self, ):
+        pass
+
+    def getSpeed(self, ):
+        pass
+
+    def accelerate(self, ):
+        pass
+
+    def stop(self, ):
+        pass
+
